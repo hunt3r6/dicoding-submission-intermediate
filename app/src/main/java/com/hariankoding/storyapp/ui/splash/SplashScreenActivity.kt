2dfe643f5e87fe35ceea3ac5af7ccee7e4ef487a
hunt3r6 <faisalmahadi126@gmail.com>
2022-10-04 14:06:28 +0700

    add hide system ui

--- app/src/main/java/com/hariankoding/storyapp/ui/splash/SplashScreenActivity.kt
+++ app/src/main/java/com/hariankoding/storyapp/ui/splash/SplashScreenActivity.kt
@@ -2,7 +2,10 @@ package com.hariankoding.storyapp.ui.splash
 
 import android.annotation.SuppressLint
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import com.hariankoding.storyapp.R
@@ -21,6 +24,7 @@ class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+        hideSystemUI()
         val token = prefHelper.fetchAuthToken()
         lifecycleScope.launch {
             delay(1000)
@@ -32,5 +36,18 @@ class SplashScreenActivity : AppCompatActivity() {
                 finish()
             }
         }
+    }
+
+    private fun hideSystemUI() {
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+        supportActionBar?.hide()
     }
 }
